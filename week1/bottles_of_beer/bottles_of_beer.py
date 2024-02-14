7517b99d7ece150abed668af359747d3185bc90d
20105828 <141300382+20105828@users.noreply.github.com>
2024-02-14 16:59:43 +0800

    Update bottles_of_beer.py

--- week1/bottles_of_beer/bottles_of_beer.py
+++ week1/bottles_of_beer/bottles_of_beer.py
@@ -1,1 +1,16 @@
+def bottles_of_beer():
+    bottles = 100
+    while True:
+        if bottles > 1:
+            print(f"{bottles} bottles of beer on the wall, {bottles} bottles of beer")
+            bottles -= 1
+        
+        elif bottles == 1:
+            print(f"{bottles} bottle of beer on the wall, {bottles} bottle of beer")
+            bottles -= 1
 
+        if bottles == 0:
+            print(f"{bottles} bottles of beer on the wall, {bottles} bottles of beer")
+            break
+
+bottles_of_beer()
