f8792f265e780189be202d65759881b4955ac51d
Robbozinoz <30688053+Robbozinoz@users.noreply.github.com>
2024-02-28 06:14:47 +0800

    feat: add weekly challenges

---
+++ week5/exercise_1/exercise_1.py
@@ -0,0 +1,31 @@
+# Define 3 helper functions for text-to-bytes conversion, bytes-to-text conversion, and byte reversal
+
+
+# Main program logic
+try:
+    # Open the binary file for reading and create output text and bytes files for writing using the context manager
+
+        # Iterate through each line in the binary file
+
+            # Decode the line to Unicode string and remove leading/trailing whitespaces
+  
+
+            # Check if the line starts with "TEXT:"
+
+                # Extract text content, convert to uppercase, and write to text file
+      
+
+            # Check if the line starts with "BYTES:"
+           
+                # Extract byte content, convert to bytes object(using fromhex()), 
+                
+                # reverse bytes, and write to bytes file
+
+    # Print success message
+    print("Conversion completed successfully!")
+
+# Handle file I/O errors
+except:
+    print("Error: Unable to open or write to file.")
+
+# Handle other exceptions using the exception class
---
+++ week5/exercise_2/exercise_2.py
@@ -0,0 +1,42 @@
+def main():
+    # try:
+        # Phase 1: File Navigation
+        print("Phase 1: File Navigation")
+        print("=======================")
+
+            # Navigate to the 5th byte position
+
+            # Read and print the next 4 bytes from the current position
+
+
+            # Move the file pointer to the beginning of the file
+
+            # Read and print the first 8 bytes from the file
+
+
+            # Print the current file pointer position
+
+
+            # Use the find() method to search for the string "ABC" in the file
+
+        
+        print("\nPhase 2: Bookmarking File Pointer")
+        print("=================================")
+        # Re-open the data.bin file in binary write-append mode
+     
+            # Use the tell() method to get the current file pointer position and store it as a bookmark
+      
+
+
+            # Write the string "XYZ" to the file
+
+
+            # Use the bookmarked pointer position to append the string "123" to the file
+   
+
+    # create three non naked exception handlers for: 
+            # not finding the file, i/o error & all other exceptions
+
+
+if __name__ == "__main__":
+    main()
