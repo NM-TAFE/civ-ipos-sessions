bcf0103ab45f078618abfc77a5fc08044d14d168
Robbo-lab <john.robertson@nmtafe.wa.edu.au>
2024-07-12 13:57:48 +0800

    fix: match activities to lesson plans for 1 & 2

--- week2/activity_1/greeter.py
+++ week2/activity_1/greeter.py
@@ -1,18 +1,15 @@
-'''Considering reuse (modularization)
+'''Considering reuse (modularisation)
 when creating python programs'''
+
 def greet():
     print("Hello, World!")
 
 # Additional activity - create a calculator class and re-use this by instantiating it in the main.py
-class Calculator:
-    def add(self, a, b):
-        return a + b
 
 # So as not to contaminate your global
 # namespace, it is good practice to use a
 # main function.
-def main():
-    greet()
+
 
 # so that we can reuse our code as both a
 # module and a script
@@ -21,4 +18,5 @@ def main():
 if __name__ == '__main__':
     print(__name__) # module name when imported
     # above is __main__ when running directly
-    main() # only called when a script
+    pass
+    # main() # only called when a script
