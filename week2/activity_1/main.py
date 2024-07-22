7abe222b6cdbbaed6c9f8b152006b66b85fe4fc3
John Robertson <ROBERJ@tdm.local>
2024-07-22 08:11:12 +0800

    feat: update session 2

--- week2/activity_1/main.py
+++ week2/activity_1/main.py
@@ -1,16 +1,12 @@
 '''different ways to reuse the same code
 as a module'''
 
-import greeter
-greeter.greet()
 
 # use an alias - consider why might we do this?
-# import greeter as g
-# g.greet()
+
 
 # sometimes we only want to import what we need
-# from greeter import greet
-# greet()
+
 
 # Additional Activity create a calculator class in the module
 
