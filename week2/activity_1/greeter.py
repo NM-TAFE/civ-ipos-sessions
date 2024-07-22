d52089f054e2716d83c8b85d61b26c9ff311db1d
20124796 <20124796@tafe.wa.edu.au>
2024-07-22 13:20:49 +0800

    complete calculator activity

--- week2/activity_1/greeter.py
+++ week2/activity_1/greeter.py
@@ -7,13 +7,36 @@ def greet():
 # Additional activity - create a calculator class and re-use this by instantiating it in the main.py
 
 
+class Calculator:
+    def __init__(self):
+        pass
+
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        return a / b
+
+
 
 # So as not to contaminate your global
 # namespace, it is good practice to use a
 # main function.
-
-
 # so that we can reuse our code as both a
 # module and a script
+
+def main():
+    greet()
+
 # we can use an if statement to ensure
 # that some code is only executed when name is __main__
+
+if __name__ == "__main__":
+    print(__name__)
+    main()
--- week2/activity_1/main.py
+++ week2/activity_1/main.py
@@ -2,19 +2,23 @@
 as a module'''
 
 import greeter
+greeter.greet()
 
 # use an alias - consider why might we do this?
-
+import greeter as g
+g.greet()
 
 # sometimes we only want to import what we need
-
+from greeter import greet
+greet()
 
 # Additional Activity create a calculator class in the module
+from greeter import Calculator
+calculator = Calculator()
+result = calculator.add(4, 5)
 
 
 # Use the new class to return a score to a user
+print(f"Result of addition: {result}")
 
 
-print(f"Result of addition: ")
-
-greeter.greet()
