8d81f4906acfc903782b7df310f77d8ba461cf6c
John Robertson <ROBERJ@tdm.local>
2024-08-12 09:26:33 +0800

    feat: update session 5

--- week5/in-class-demos/in-class-demos.py
+++ week5/in-class-demos/in-class-demos.py
@@ -21,24 +21,24 @@
 # # binary_representation = ' '.join([bin(ord(c))[2:].zfill(8) for c in "Everything is Binary"])
 # # print(binary_representation)
 
-# # # Simpler explanation of bytes
-# binary_representations = []
+# # Simpler explanation of bytes
+binary_representations = []
 
-# for c in "Everything is Binary":
-#     # Step 1: Get the Unicode code point of the character
-#     unicode_code_point = ord(c)
-#     print(f"Unicode point: {unicode_code_point}")
+for c in "Everything is Binary":
+    # Step 1: Get the Unicode code point of the character
+    unicode_code_point = ord(c)
+    print(f"Unicode point: {unicode_code_point}")
 
-#     # Step 2: Convert the Unicode code point to binary and remove the prefix '0b'
-#     binary_representation = bin(unicode_code_point)[2:]
-#     print(f"Binary: {binary_representation}")
+    # Step 2: Convert the Unicode code point to binary and remove the prefix '0b'
+    binary_representation = bin(unicode_code_point)[2:]
+    print(f"Binary: {binary_representation}")
 
-#     # Step 3: Fill the binary string with leading zeros to ensure it's 8 characters long
-#     padded_binary_representation = binary_representation.zfill(8)
-#     print(f"Padded binary: {padded_binary_representation}")
+    # Step 3: Fill the binary string with leading zeros to ensure it's 8 characters long
+    padded_binary_representation = binary_representation.zfill(8)
+    print(f"Padded binary: {padded_binary_representation}")
 
-#     # Append the padded binary representation to the list
-#     binary_representations.append(padded_binary_representation)
+    # Append the padded binary representation to the list
+    binary_representations.append(padded_binary_representation)
 
 # # Print the binary representations
 # for binary_repr in binary_representations:
