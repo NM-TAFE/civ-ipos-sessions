57f4ccd8a58b42281ee28378e777a55c4cdd2ee6
John Robertson <ROBERJ@tdm.local>
2024-08-26 08:04:11 +0800

    feat: binary search

--- week6/in_class_demos/searching_sorting/binary_search.py
+++ week6/in_class_demos/searching_sorting/binary_search.py
@@ -1,7 +1,16 @@
 def binary_search(temperatures, target):
     # Binary search that calculates the middle index mid and compares the temperature 
     # at index mid with the target temperature.
-    pass
+    left, right = 0, len(temperatures)-1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if temperatures[mid] == target:
+            return mid
+        elif temperatures[mid] < target:
+            left =  mid + 1
+        else:
+            right = mid - 1
 
 # Setup & call
 temperatures = [20, 21, 22, 22, 23, 23, 24, 25, 25, 25, 24, 23, 22, 21, 20, 19, 18, 18, 17, 16, 16, 15, 14, 13]
