4ff565e9797b20ebbab37a3c0c6cbc9b63695197
Robbo-lab <john.robertson@nmtafe.wa.edu.au>
2025-08-27 10:16:39 +0800

    feat: update circular reference in collections demo

--- week6/in_class_demos/data_structures/stacks_queues/collections_library_demo.py
+++ week6/in_class_demos/data_structures/stacks_queues/collections_library_demo.py
@@ -2,7 +2,7 @@ from collections import deque
 
 class Queue:
     def __init__(self):
-        self.queue= deque()
+        self.queue = deque()
 
     def peek(self):
         if not self.is_empty():
@@ -20,21 +20,19 @@ class Stack:
         if not self.is_empty():
             return self.stack[-1]
         else:
-            return None# Return None if the queue is empty
+            return None # Return None if the queue is empty
     
     def is_empty(self):
         return len(self.stack) == 0
 
 # Example usage:
-my_queue =Queue()
-my_stack =Stack()
+my_queue = Queue()
+my_stack = Stack()
 my_queue.queue.append(1)
 my_stack.stack.append(2)
-# my_queue.queue.append(3)
 my_queue.queue.extendleft([-1, -2, -3])
 my_stack.stack.extend([4,5,6])
 
-my_queue
-
 print("Popped item:", my_stack.stack.pop()) # Output: Popped item: 3
-print("Popped item:", my_queue.queue.popleft()) # Output: Popped item: 2    
+print("Popped item:", my_queue.queue.popleft()) # Output: Popped item: 2
+print(f"Available methods:\n append appendleft\n clear count\n extend extendleft\n pop popleft\n remove reverse rotate")    
